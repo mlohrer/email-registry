cc0a480d306df6d8f57f0768d6e5cb7f1730dee8
Martin Lohrer <promalo@gmail.com>
2021-05-03 02:44:25 +0100

    Not an automated change by any means

--- src/main.js
+++ src/main.js
@@ -35,6 +35,7 @@ const filterAddressList = () => {
         );
     });
 };
+// Test: assert addressList ['ab', 'bc', 'cd'] going through filterAddressList() with filterList [{value: 'c', includes: 'true'}] equals ['bc', 'cd']
 
 const writeAddressListToDom = async () => {
     try {
@@ -53,6 +54,7 @@ const writeAddressListToDom = async () => {
         console.log(err);
     }
 }
+// Test: after executing writeAddressListToDom, document.getElementByID('address-list').children.forEach (item => assert addressList.indexOf(item.innerHTML) >= 0)
 
 const writeFilterListToDom = async () => {
     try {
@@ -72,6 +74,8 @@ const writeFilterListToDom = async () => {
         console.log(err);
     }
 }
+// Test: after executing writeFilterListToDom with filterList [{value:'a', includes: true}, {value:'b', includes: false}], execute
+// document.getElementByID('filter-list').children.forEach (item => assert filterList.indexOf(item.innerHTML) >= 0)
 
 window.addEventListener('load', async () => {
     try {
@@ -83,19 +87,26 @@ window.addEventListener('load', async () => {
     }
 })
 
+
 const validateEmail = async (address) => {
     try {
         let response = await fetch(`https://api.trumail.io/v2/lookups/json?email=${address}`);
+        let data = response.json();
         if (!response.ok) {
             throw new Error(`HTTP Error. Status: ${response.status}`);
+        };
+        if ((!data.validFormat === true) || (!data.deliverable === true)) {
+            throw new Error(`Invalid eMail address, please try again.`)
+        };
+        if (data['Message']) {
+            throw new Error(`You've submitted too many eMail addresses. Please try again in five minutes.`);
         }
-        return response;
+        return data;
     } catch (err) {
         console.log(err);
     }
 }
-
-// validateEmail('promalo@gmail.com').then(data => console.log(data));
+// Test: validateEmail('bubblywobbly').then(response => response.json()).then(data => assert (data.validFormat === true) && (data.deliverable === true))
 
 const addEmailToDom = async () => {
     try {
@@ -106,11 +117,12 @@ const addEmailToDom = async () => {
             document.getElementById('email-input').innerHTML = '';
             writeAddressListToStorage();
             toggleModal();
-        }
+        };
     } catch (err) {
         console.log(err);
     }
 }
+// Test: after executing addEmailToDom with test@test.com, assert document.getElementByID('filter-list').children[children.length - 1] equals filterList[filterList.length - 1]
 
 document.getElementById('email-form').addEventListener('submit', function (event) {
     event.preventDefault();
@@ -123,3 +135,9 @@ document.getElementById('add-user').addEventListener('click', toggleModal);
 document.getElementById('modal-close').addEventListener('click', toggleModal);
 
 document.getElementById('add-email').addEventListener('click', addEmailToDom);
+
+// Solutions for filtering addressList with an ordered chain of expressions containing an arbitrary number of AND/OR string filters:
+// 1. Rewrite filterAddressList() to use addressList.filter(expression1 && expression2 || expression 3 || expression4 && expression 5)
+// 2. Run filterAddressList() for all &&-linked filter expressions (grouped up through a for-loop with groupCount++ for every new || expression), then concat() all resulting arrays
+
+// The first solution might not actually be faster in all browsers, esp. those not running on V8, since it relies on a browser-specific implementation of the new JS spec feature filter()
